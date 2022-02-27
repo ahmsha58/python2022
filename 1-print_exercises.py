cf772550117a40302e219a07b31836bc93a534e3
ahmsha58 <100097104+ahmsha58@users.noreply.github.com>
2022-02-28 09:50:52 +1300

    compleated

---
+++ 1-print_exercises.py
@@ -0,0 +1,23 @@
+#1
+#print out your name1
+print("Ahmad shahoud")
+#2
+print("luck smith")
+#3
+print("kings high school")
+#4
+print("16")
+#5
+print(2022+25)
+#6
+print(100+16)
+#7
+print(658/7)
+#8
+print(2022-1969)
+#9
+print("fifa 22")
+#10
+print ("english")
+#
+
