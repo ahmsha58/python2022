316ddb01d940c252a304e7d03e4cfccc76a4f841
ahmsha58 <100097104+ahmsha58@users.noreply.github.com>
2022-03-02 12:21:10 +1300

    Create 4_modifyingvariables.py

---
+++ 4_modifyingvariables.py
@@ -0,0 +1,23 @@
+money = 200
+hat = 20
+top = 30
+pants = 15
+belt = 60
+shoes = 40
+print(money)
+print("buying a hat")
+money  = money - hat
+print("you have 180 now")
+print("buying a top")
+money = money - top
+print("you have 150 now")
+print("buying a pants")
+money = money - pants
+print("you have 135 now")
+print("buying a belt")
+money = money - belt
+print("you have 75 now")
+print("buying a shoes")
+money = money - shoes 
+print("you have 35 now")
+
